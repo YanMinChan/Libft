9566c2e754d3b9268983f92b13473d06f8d84fd3
Yan min Chan <ychan@u91z04s06.42kl.edu.my>
2021-08-07 16:30:34 +0800

    ft_split finally

--- ft_split.c
+++ ft_split.c
@@ -1,51 +1,101 @@
-//#include "libft.h"
 #include <stdlib.h>
 
-static size_t	ft_strlen(const char *s)
+//function to count how many word//
+//function to extract word
+//function to count wordlen
+//malloc to assign mem
+//put word to arr
+
+static int	ft_wrdnbr(char const *s, char c)
 {
-	size_t	len;
+	int	wrdnbr;
+	
+	if (!s)
+		return (0);
+	wrdnbr = 0;
+	while (*s && *s == c)
+		s++;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			wrdnbr++;
+			while (*s && *s == c)
+				s++;
+			continue ;
+		}
+		s++;
+	}
+	if (*(s - 1) != c)
+		wrdnbr++;
+	return (wrdnbr);
+}
+
+static int	ft_wrdlen(char const *s, char c)
+{
+	int	len;
 
 	len = 0;
-	while (s[len])
+	while (*s && *s == c)
+		s++;
+	while (s[len] && s[len] != c)
 		len++;
 	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**arr;
-	int		i;
-	int		j;
+	char **arr;
+	int	i;
 
-	i = 0;
-	j = 0;
-	if (!s)
+	arr = (char **)malloc(sizeof(*arr) * (ft_wrdnbr(s, c) + 1));
+	if (!arr)
 		return (0);
-	arr = (char **)malloc(sizeof(**arr) * (ft_strlen(s) + 1));
+	i = 0;
 	while (*s)
 	{
-		arr[i] = (char *)malloc(sizeof(*arr) * (ft_strlen(s) + 1));
 		if (*s == c)
+			s++;
+		if (*s != c)
 		{
+			arr[i] = (char *)malloc(sizeof(arr) * (ft_wrdlen(s, c) + 1));
+			if (!arr[i])
+				break ;
+			ft_strlcpy(arr[i], s, ft_wrdlen(s, c) + 1);
+			s += ft_wrdlen(s, c);
 			i++;
-			j = 0;
 		}
-		arr[i][j++] = *s++;
 	}
-	arr[i][j] = '\0';
+	arr[i] = 0;
 	return (arr);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int	main(void)
 {
-	const char s[] = "ABC DEF GHI";
+	const char *s = "      Hello      its me leh eh      ";
 	char	c = ' ';
-	char	**arr = ft_split(s, c);
 
-	for(int i = 0; i < 3; i++)
+	while (*s)
 	{
-		printf("%s ", arr[i]);
+	   if (*s == c)
+		   s++;
+	   if (*s != c)
+	   {
+		   printf("%d\n", ft_wrdlen(s, c));
+		   s += ft_wrdlen(s, c);
+	   }
 	}
 }
+
+int	main(void)
+{
+	const char *s = "Hello its me";
+	char	c = ' ';
+	char	**arr;
+
+	arr = ft_split(s, c);
+	for(int i = 0; i < ft_wrdnbr(s, c); i++)
+		printf("%s    ", arr[i]);
+}*/
